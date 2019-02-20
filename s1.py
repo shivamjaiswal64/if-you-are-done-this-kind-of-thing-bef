09f7c6d3dbf76512ef55357d6abab48b0eea0c09
SHIVAM JAISWAL <shivamjaiswal64@gmail.com>
2019-02-20 18:13:44 +0530

    initial commit

---
+++ s1.py
@@ -0,0 +1,7 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #python
+>>> print("Hello")
+>>> a=5
+>>> b=6
+>>> print(a+b)
